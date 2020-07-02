34d267de1bbe9fc53aef336695d0ff0eefd4b41d
matgd <matgd@users.noreply.github.com>
2020-07-02 18:40:08 +0200

    Added untested installation

--- Vim/apt/install.sh
+++ Vim/apt/install.sh
@@ -3,10 +3,31 @@
 CYAN='\033[1;36m'
 NC='\033[0m'
 
+VIMRC_LOCATION="~/.vimrc"
+
 install_vim=0
 install_nerdtree=1
 install_fzf=$(which fzf > /dev/null; echo $?)
 setup_vimrc=1
+
+function installVim {
+  sudo apt-get update 
+  sudo apt-get install vim
+}
+
+function installNERDTree {
+  git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+  vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+}
+
+function installFuzzyFinder {
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+}
+
+function setupVimrc {
+  curl -s https://raw.githubusercontent.com/matgd/editors-configs/master/Vim/.vimrc > "${VIMRC_LOCATION}"
+}
 
 function binaryToYesNo {
   BLUE='\033[1;34m'
@@ -50,7 +71,11 @@ while [[ ! ${answer} == 0 ]]; do
     4) setup_vimrc=$((${setup_vimrc} ^ 1))
        ;;
     0) echo OK
-       ;; 
+       [[ ${install_vim} -eq 1 ]] && installVim
+       [[ ${install_nerdtree} -eq 1 ]] && installNERDTree
+       [[ ${install_fzf} -eq 1 ]] && installFuzzyFinder
+       [[ ${setup_vimrc} -eq 1 ]] && setupVimrc
+       ;;
     99) exit 99
         ;;
   esac
